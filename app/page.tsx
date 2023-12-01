fefa8a0a2c0b0476bd2d722a2c394bd8a3e58306
Nivethan1999 <m-iquaz@hotmail.com>
2023-12-01 10:59:45 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,6 @@ export default async function HomePage(){
     const session = await getServerSession(authOptions)
     const userRole = session?.user?.role
     return (<h1>
-            "hi mom" {userRole}
+            hi mom {userRole}
     </h1> );
 }
