541b18f52a36dc6750d1e722f4c8b1ed869e446d
Nivethan1999 <m-iquaz@hotmail.com>
2023-12-01 10:42:28 +0100

    Manager can create trainer

--- app/manager/page.tsx
+++ app/manager/page.tsx
@@ -1,7 +1,31 @@
+// import { checkSession } from "@/services/TrainerService";
+// import CreatePersonalTrainerForm from "../../components/CreatePersonalTrainer";
+
+// export default async function ManagerPage() {
+//   const session = await checkSession();
+//   return <CreatePersonalTrainerForm />;
+// }
+
+import Layout from "@/components/Layout";
+import CreatePersonalTrainerForm from "../../components/CreatePersonalTrainer";
 import { checkSession } from "@/services/TrainerService";
-import CreatePersonalTrainerForm from "./CreatePersonalTrainer";
+import { redirect } from "next/navigation";
 
 export default async function ManagerPage() {
   const session = await checkSession();
-  return <CreatePersonalTrainerForm />;
+  if (session?.role?.toLowerCase() !== "manager") {
+    redirect("/");
+  }
+  return (
+    <main className="px-4 flex min-h-screen flex-col items-center justify-between">
+      <div className="p-4 min-w-full">
+        <div className="bg-blue-200 p-6 shadow-2xl border-4 border-blue-100-300 rounded-t-2xl mb-12">
+          <h2 className="text-4xl text-center text-gray-800 pb-8">
+            Opret en Personal Trainer
+          </h2>
+          <CreatePersonalTrainerForm />
+        </div>
+      </div>
+    </main>
+  );
 }
--- services/ManagerService.tsx
+++ services/ManagerService.tsx
@@ -1,32 +1,62 @@
+"use server";
 import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 import axios from "axios";
 import { getServerSession } from "next-auth";
 import { revalidateTag } from "next/cache";
 
-export async function createPersonalTrainer(
-    prevState:any,
-    formData: FormData,
-  ) {
-    const url = "https://afefitness2023.azurewebsites.net/api/Users";
-    const session = await getServerSession(authOptions);
-    const newPersonalTrainer = {
-      userId: 0,
-      firstName: formData.get("firstName"),
-      lastName: formData.get("lastName"),
-      email: formData.get("email"),
-      password: formData.get("password"),
-      accountType: "PersonalTrainer",
-    };
+// export async function createPersonalTrainer(
+//     prevState:any,
+//     formData: FormData,
+//   ) {
+//     const url = "https://afefitness2023.azurewebsites.net/api/Users";
+//     const session = await getServerSession(authOptions);
+//     const newPersonalTrainer = {
+//       userId: 0,
+//       firstName: formData.get("firstName"),
+//       lastName: formData.get("lastName"),
+//       email: formData.get("email"),
+//       password: formData.get("password"),
+//       accountType: "PersonalTrainer",
+//     };
   
-    try {
-      await axios.post(url, newPersonalTrainer, {
-        headers: {
-          Authorization: `Bearer ${session?.user?.token}`,
-        },
-      });
-      revalidateTag("clients");
-      return { message: "Personal Trainer created successfully", success: true };
-    } catch (error) {
-      return { message: `Failed with error: ${error}`, success: false };
-    }
+//     try {
+//       await axios.post(url, newPersonalTrainer, {
+//         headers: {
+//           Authorization: `Bearer ${session?.user?.token}`,
+//         },
+//       });
+//       revalidateTag("clients");
+//       return { message: "Personal Trainer created successfully", success: true };
+//     } catch (error) {
+//       return { message: `Failed with error: ${error}`, success: false };
+//     }
+//   }
+
+
+export async function createPersonalTrainer(
+  prevState: any,
+  formData: FormData,
+) {
+  const url = "https://afefitness2023.azurewebsites.net/api/Users";
+  const session = await getServerSession(authOptions);
+  const newPersonalTrainer = {
+    userId: 0,
+    firstName: formData.get("firstName"),
+    lastName: formData.get("lastName"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+    accountType: "PersonalTrainer",
+  };
+
+  try {
+    await axios.post(url, newPersonalTrainer, {
+      headers: {
+        Authorization: `Bearer ${session?.user?.token}`,
+      },
+    });
+    revalidateTag("clients");
+    return { message: "Personal Trainer created successfully", success: true };
+  } catch (error) {
+    return { message: `Failed with error: ${error}`, success: false };
   }
+}
